50d8222666c310016a30bb16645dbba255203746
MCiel-Costa <mcl1997@gmail.com>
2026-06-10 10:35:48 -0300

    Add files via upload

--- robo_inventario.py
+++ robo_inventario.py
@@ -46,11 +46,13 @@ try:
     
     # Adicionando modo Headless para rodar na nuvem
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless=new')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
     chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     chrome_options.page_load_strategy = 'eager'
 
     servico = ChromeService()

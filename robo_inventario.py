7ad5549639fd3f317d361dd2ec78fcc49894181f
MCiel-Costa <mcl1997@gmail.com>
2026-06-10 10:47:09 -0300

    Add files via upload

--- robo_inventario.py
+++ robo_inventario.py
@@ -50,10 +50,10 @@ try:
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-software-rasterizer')
     chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--blink-settings=imagesEnabled=false') # Não carrega imagens para ser mais rápido
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-    chrome_options.page_load_strategy = 'eager'
 
     servico = ChromeService()
     navegador = webdriver.Chrome(service=servico, options=chrome_options)
